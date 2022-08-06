87729820752adaef23b588f46dfd4380365a9574
Gabriel <j_gabrielorozco@live.com>
2022-08-06 11:40:33 -0500

    Función main del receptor escrita.

--- marconiProject.cpp
+++ marconiProject.cpp
@@ -5,27 +5,28 @@
 #include "nrf24l01_driver.h"
 
 #define SPI_PORT    spi0
+#define BUF_LEN 32
+
+char buffer[BUF_LEN];
+
+void printBuff(){
+    for (uint8_t i = 0; i < BUF_LEN; i++)
+    {
+        printf("%c", buffer[i]);
+    }
+}
 
 int main()
 {
     nrf24l01_driver NRF(SPI_PORT, 5, 6);
     NRF.default_config();
-    NRF.goTo_tx();
-
-    char buffer[32];
+    NRF.goTo_rx();
 
     while(1){
-        sprintf(buffer, "60");
-        buffer[30] = 'R';
-        buffer[31] = 'O';
-        NRF.send(buffer);
-        sleep_ms(3000);
-
-        sprintf(buffer, "-60");
-        buffer[30] = 'R';
-        buffer[31] = 'O';
-        NRF.send(buffer);
-        NRF.send(buffer);
-        sleep_ms(3000);
+        if(NRF.dataInc()){
+            printf("Datos recibidos...\n");
+            NRF.receive(buffer);
+            printBuff();
+        }
     }
 }
