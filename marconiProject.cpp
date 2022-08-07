ca6a4bfd90580a6198cf91350f045c2723b8f999
Juan Gabriel Orozco <jgabriel.orozco@udea.edu.co>
2022-08-07 15:04:47 -0500

    Se leen y se envían los grados del slider, para controlar la vela.

--- marconiProject.cpp
+++ marconiProject.cpp
@@ -3,6 +3,9 @@
 #include "hardware/spi.h"
 #include "nrf24l01.h"
 #include "nrf24l01_driver.h"
+#include "hardware/adc.h"
+#include "hardware/pwm.h"
+#include "hardware/clocks.h"
 
 #define SPI_PORT    spi0
 #define BUF_LEN 1
@@ -11,22 +14,44 @@
 #define SERVO_MASK 0x01
 #define MODE_MASK 0x02
 
+//Modo operación (automático, control remoto)
+const int opModePin = 10;
+bool opMode = true;
+
+//Para controlar el servo2
+bool rightButton = false;
+bool leftButton = false;
+const int rightPin = 12;
+const int leftPin = 15;
+
+//Para control de servo1
+const int timonPin = 28;
+uint16_t potenciometro;
+float grados1;
+
+//Factor de converion del adc
+const float conversion_factor = 3.3f / (1 << 12);
+
 
 //EMISOR
 char buffer[BUF_LEN];
-uint8_t angulo = 0xf5;
+uint8_t val2send = 0xf5;
 
 void printBuff(){
     for (uint8_t i = 0; i < BUF_LEN; i++)
     {
-        printf("%X", buffer[i]);
+        printf("%u", buffer[i]);
     }
     printf("\n");
 }
 
+void setPeripherals();
+void readVelaDegree();
+
 int main()
 {
-    stdio_init_all();
+    stdio_init_all();//Inicializar input-output
+    setPeripherals();//Inicializar perifericos
     sleep_ms(5000);
     nrf24l01_driver NRF(SPI_PORT, 5, 6);
     
@@ -37,18 +62,44 @@ int main()
 
     while(1){
         
-        //trama[0] = 180;
-        //trama[1] = 0;
-
-        sprintf(buffer, "%c", (char*)angulo);
+        readVelaDegree();
+        val2send = grados1;
+        sprintf(buffer, "%c", (char*)val2send);
         
-        printf("Enviando datos...\n");
         NRF.send(buffer);
         printf("Se envió: \n");
         printBuff();
-        sleep_ms(5000);
+        sleep_ms(1000);
     }
 }
+
+void setPeripherals(){
+    //SYS_INIT
+    adc_init();
+    stdio_init_all();
+
+    //Modo de operación
+    gpio_init(opModePin);
+    gpio_set_dir(opModePin, GPIO_IN);
+    
+    //Potenciometro
+    adc_gpio_init(timonPin);
+    adc_select_input(2);
+
+    //Pulsadores
+    gpio_init(rightPin);
+    gpio_init(leftPin);
+    gpio_set_dir(rightPin, GPIO_IN);
+    gpio_set_dir(leftPin, GPIO_IN); 
+}
+
+void readVelaDegree(){
+    potenciometro = adc_read();
+    //printf("Raw value: 0x%03x, voltage: %f V\n", potenciometro, potenciometro * conversion_factor);
+    grados1 = (potenciometro * conversion_factor) * (55);
+    printf("Los grados medidos son: %f\n", grados1);
+}
+
 
 //RECEPTOR
 // char buffer[BUF_LEN];
